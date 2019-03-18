6be216da417ec0ccbe66fe1208441a6e842f2ce8
laurenkerns <laurenkerns@gmail.com>
2019-03-18 14:11:53 -0400

    inital file structure

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the database
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table burger
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT,
+  burger_name varchar(30) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
