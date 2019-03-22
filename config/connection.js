2f1b685c04a962b71a460c524ef8293a3d95def8
laurenkerns <laurenkerns@gmail.com>
2019-03-22 09:01:16 -0400

    server.js edits

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+const path = require("path");
 
 //create connection
 let connection;
--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
+const path = require("path")
 
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // /////// JAWSDB H13 ERROR SOLUTION://////
 // app.use(timeout(15000));
